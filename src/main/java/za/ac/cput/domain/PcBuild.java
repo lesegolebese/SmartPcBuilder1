2e92645d6c39b7182ab7585a059de969d22c528a
Mnelisi17 <222062088@mycput.ac.za>
2026-06-22 00:38:46 +0200

    Added PcBuild and PcBuildItem domain classes

---
+++ src/main/java/za/ac/cput/domain/PcBuild.java
@@ -0,0 +1,108 @@
+/* PcBuild.java
+   PcBuild POJO entity implementing Builder Pattern
+   Author: Mnelsi Mabona (222062088)
+   Date: 21 June 2026
+*/
+
+package za.ac.cput.domain;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PcBuild {
+    private String buildId;
+    private String buildName;
+    private LocalDate createdDate;
+    private User user;
+    private List<PcBuildItem> buildItems;
+
+    protected PcBuild() {}
+
+    protected PcBuild(Builder builder) {
+        this.buildId = builder.buildId;
+        this.buildName = builder.buildName;
+        this.createdDate = builder.createdDate;
+        this.user = builder.user;
+        this.buildItems = builder.buildItems != null
+                ? builder.buildItems
+                : new ArrayList<>();
+    }
+
+    public String getBuildId() {
+        return buildId;
+    }
+
+    public String getBuildName() {
+        return buildName;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public List<PcBuildItem> getBuildItems() {
+        return buildItems;
+    }
+
+    @Override
+    public String toString() {
+        return "PcBuild{" +
+                "buildId='" + buildId + '\'' +
+                ", buildName='" + buildName + '\'' +
+                ", createdDate=" + createdDate +
+                ", user=" + user +
+                ", buildItems=" + buildItems +
+                '}';
+    }
+
+    public static class Builder {
+        private String buildId;
+        private String buildName;
+        private LocalDate createdDate;
+        private User user;
+        private List<PcBuildItem> buildItems;
+
+        public Builder setBuildId(String buildId) {
+            this.buildId = buildId;
+            return this;
+        }
+
+        public Builder setBuildName(String buildName) {
+            this.buildName = buildName;
+            return this;
+        }
+
+        public Builder setCreatedDate(LocalDate createdDate) {
+            this.createdDate = createdDate;
+            return this;
+        }
+
+        public Builder setUser(User user) {
+            this.user = user;
+            return this;
+        }
+
+        public Builder setBuildItems(List<PcBuildItem> buildItems) {
+            this.buildItems = buildItems;
+            return this;
+        }
+
+        public Builder copy(PcBuild pcBuild) {
+            this.buildId = pcBuild.buildId;
+            this.buildName = pcBuild.buildName;
+            this.createdDate = pcBuild.createdDate;
+            this.user = pcBuild.user;
+            this.buildItems = pcBuild.buildItems;
+            return this;
+        }
+
+        public PcBuild build() {
+            return new PcBuild(this);
+        }
+    }
+}
---
+++ src/main/java/za/ac/cput/domain/PcBuildItem.java
@@ -0,0 +1,74 @@
+/* PcBuildItem.java
+   PcBuildItem POJO entity implementing Builder Pattern
+   Author: Mnelsi Mabona (222062088)
+   Date: 21 June 2026
+*///
+
+package za.ac.cput.domain;
+
+public class PcBuildItem {
+    private String buildItemId;
+    private int quantity;
+    private Product product;
+
+    protected PcBuildItem() {}
+
+    protected PcBuildItem(Builder builder) {
+        this.buildItemId = builder.buildItemId;
+        this.quantity = builder.quantity;
+        this.product = builder.product;
+    }
+
+    public String getBuildItemId() {
+        return buildItemId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    @Override
+    public String toString() {
+        return "PcBuildItem{" +
+                "buildItemId='" + buildItemId + '\'' +
+                ", quantity=" + quantity +
+                ", product=" + product +
+                '}';
+    }
+
+    public static class Builder {
+        private String buildItemId;
+        private int quantity;
+        private Product product;
+
+        public Builder setBuildItemId(String buildItemId) {
+            this.buildItemId = buildItemId;
+            return this;
+        }
+
+        public Builder setQuantity(int quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+
+        public Builder setProduct(Product product) {
+            this.product = product;
+            return this;
+        }
+
+        public Builder copy(PcBuildItem item) {
+            this.buildItemId = item.buildItemId;
+            this.quantity = item.quantity;
+            this.product = item.product;
+            return this;
+        }
+
+        public PcBuildItem build() {
+            return new PcBuildItem(this);
+        }
+    }
+}
