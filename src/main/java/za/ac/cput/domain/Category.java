48868b156146158f2b5080a994d9b99476d2317a
Lerato-25 <letsoelelerato@gmail.com>
2026-06-21 21:24:13 +0200

    Created Product, Category and Specification entities using Builder Pattern

---
+++ src/main/java/za/ac/cput/domain/Category.java
@@ -0,0 +1,77 @@
+package za.ac.cput.domain;
+
+/* Category.java
+   Category POJO class
+
+   Author: Lerato(222381582)
+
+   Date: 21 June 2026
+*/
+
+public class Category {
+
+    private Long categoryId;
+    private String name;
+    private String description;
+
+    protected Category() {}
+
+    protected Category(Builder builder) {
+        this.categoryId = builder.categoryId;
+        this.name = builder.name;
+        this.description = builder.description;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private Long categoryId;
+        private String name;
+        private String description;
+
+        public Builder setCategoryId(Long categoryId) {
+            this.categoryId = categoryId;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder copy(Category category) {
+            this.categoryId = category.categoryId;
+            this.name = category.name;
+            this.description = category.description;
+            return this;
+        }
+
+        public Category build() {
+            return new Category(this);
+        }
+    }
+}
---
+++ src/main/java/za/ac/cput/domain/Specification.java
@@ -0,0 +1,78 @@
+package za.ac.cput.domain;
+
+/* Specification.java
+   Specification POJO class
+
+   Author: Lerato (222381582)
+
+   Date: 21 June 2026
+*/
+
+
+public class Specification {
+
+    private Long specificationId;
+    private String key;
+    private String value;
+
+    protected Specification() {}
+
+    protected Specification(Builder builder) {
+        this.specificationId = builder.specificationId;
+        this.key = builder.key;
+        this.value = builder.value;
+    }
+
+    public Long getSpecificationId() {
+        return specificationId;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Specification{" +
+                "specificationId=" + specificationId +
+                ", key='" + key + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private Long specificationId;
+        private String key;
+        private String value;
+
+        public Builder setSpecificationId(Long specificationId) {
+            this.specificationId = specificationId;
+            return this;
+        }
+
+        public Builder setKey(String key) {
+            this.key = key;
+            return this;
+        }
+
+        public Builder setValue(String value) {
+            this.value = value;
+            return this;
+        }
+
+        public Builder copy(Specification specification) {
+            this.specificationId = specification.specificationId;
+            this.key = specification.key;
+            this.value = specification.value;
+            return this;
+        }
+
+        public Specification build() {
+            return new Specification(this);
+        }
+    }
+}
