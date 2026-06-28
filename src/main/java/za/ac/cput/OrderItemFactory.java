1ae2002e5512d72349c4e7f5331c7f6c34a5727d
Coben220235686 <220235686@mycput.ac.za>
2026-06-28 18:23:52 +0200

    Update OrderItemFactory.java

--- src/main/java/za/ac/cput/OrderItemFactory.java
+++ src/main/java/za/ac/cput/OrderItemFactory.java
@@ -1,1 +1,26 @@
 
+/*
+ * OrderItemFactory.java
+ * Author: Coben Maistry
+ * Date: 28 June 2026
+ */
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.OrderItem;
+import za.ac.cput.util.Helper;
+
+public class OrderItemFactory {
+
+    public static OrderItem createOrderItem(String orderItemId, int quantity, double unitPrice) {
+
+        if (Helper.isNullOrEmpty(orderItemId) || quantity <= 0 || unitPrice <= 0) {
+            return null;
+        }
+
+        return new OrderItem.Builder()
+                .setOrderItemId(orderItemId)
+                .setQuantity(quantity)
+                .setUnitPrice(unitPrice)
+                .build();
+    }
+}
