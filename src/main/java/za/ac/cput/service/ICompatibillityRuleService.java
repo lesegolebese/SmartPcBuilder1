8a5479b46d3fd39505bacc4d66984610057a9b2e
Matinisa Lubisi <matinisavunywalive@gmail.com>
2026-07-12 17:53:50 +0200

    Just did and added adminService and compatibillityRuleService, along with their interfaces

---
+++ src/main/java/za/ac/cput/service/ICompatibillityRuleService.java
@@ -0,0 +1,13 @@
+package za.ac.cput.service;
+
+import za.ac.cput.domain.CompatibilityRule;
+
+import java.util.List;
+
+public interface ICompatibillityRuleService extends Service<CompatibilityRule, String> {
+    List<CompatibilityRule> getAll();
+    List<CompatibilityRule> findByComponentType1(String componentType1);
+    List<CompatibilityRule> findByDescription(String description);
+    List<CompatibilityRule> findByRuledescriptionContaining(String keyword);
+    CompatibilityRule findByRuleId(String ruleId);
+}
